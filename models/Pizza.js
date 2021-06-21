1a92ed80523b02cf621df1cbfcccb8e686bba919
[Andrew Core] <[adcore89@gmail.com]>
2021-06-21 19:06:56 -0400

    pizza model started

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,1 +1,21 @@
 const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema ({
+
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppinfs: []
+});
+
