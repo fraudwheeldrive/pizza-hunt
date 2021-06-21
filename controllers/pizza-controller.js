37ac94a19ef51c3bb959b691354bf93bf11471dc
[Andrew Core] <[adcore89@gmail.com]>
2021-06-21 19:57:09 -0400

    pizza controllers built, its time to create some routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -2,6 +2,34 @@ const { Pizza } = require('../models');
 
 const pizzaController = {
     // the functions will go in here as methods
+
+    //get all pizza
+    getAllPizza(req, res) {
+        Pizza.find({})
+        .then(dbPizzaData => res.json(dbPizzaData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    };
+
+// get one pizza by id 
+
+getPizzaById({ params }, res) {
+    Pizza.findOne({_id: params.id })
+    .then(dbPizzaData => {
+        //if no pizza is found, send 404
+        if (!dbPizzaData) {
+            res.status(404).json({ message: 'no pizza found with this id' });
+        }
+        res.json(dbPizzaData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err)
+    });
+},
+
 };
 
 module.exports = pizzaController;
