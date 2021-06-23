3702de18cb1f90f12575abc043337e02afddeabe
[Andrew Core] <[adcore89@gmail.com]>
2021-06-22 20:59:18 -0400

    comment controll add and delete

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,13 +1,49 @@
 const { Comment, Pizza } = require('../models');
 
 const commentController = {
-    addComment() {
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+        .then(({_id }) => {
+            return Pizza.findOneAndUpdate(
+                {_id: params.pizzaID },
+                { $push: { comments: _id } },
+                {new: true }
+            );
+        })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!'});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
 
     },
 
-    removeComment() {
-
+    removeComment ({ params }, res) {
+        Comment.findOneAndDelete({_id: params.commentID })
+        .then(deletedComment => {
+            if (!deletedComment) {
+                return res.status(404).json({ message: "no comment with this id!"})
+            }
+            return Pizza.findOneAndUpdate(
+                {_id: params.pizzaID },
+                { $pull: {comments: params.commentID } },
+                { new: true }
+            );
+        });
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this ID '});
+                return;
+            }
+            res.json(dbPizzaData);
+        })
     }
-};
+
+}
+
 
 module.exports = commentController;
