836feb61a645fc2d7edc9246b6c2fbe514e4f86f
[Andrew Core] <[adcore89@gmail.com]>
2021-06-23 19:10:31 -0400

    connection trouble shot

--- models/index.js
+++ models/index.js
@@ -1,5 +1,46 @@
-const Pizza = require ('./Pizza');
-const Comment = require('./Comment');
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-module.exports = { Pizza, Comment };
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+      }
+    ]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
 
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+const Pizza = model('Pizza', PizzaSchema);
+
+module.exports = Pizza;
